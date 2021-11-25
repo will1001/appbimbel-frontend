d3ee5efec66e35c517906b2c81ccf09ecd2f482b
will1001 <wilirahmatm@gmail.com>
2021-11-25 10:06:43 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/appbimbel-frontend/precache-manifest.36874f0237363f33aa99515dc2bfe0ce.js"
+  "/appbimbel-frontend/precache-manifest.1961e918677bd8bf12b889d45d1b2fe4.js"
 );
 
 self.addEventListener('message', (event) => {
